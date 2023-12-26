21237d47cd3373e05a5de75a87ae870a8d9747ed
Pardeep Bhasin <pardeep.bhasin@nagarro.com>
2023-12-26 19:31:05 +0530

    fix schema validator

--- src/lib/model/schemaValidators.ts
+++ src/lib/model/schemaValidators.ts
@@ -17,6 +17,7 @@ export const cartSchemaValidator = z.object({
     description: z.string(),
     price: z.number(),
     ingredients: z.array(z.string()),
-    image: z.string()
+    image: z.string(),
+    status: z.string()
 }
 )
